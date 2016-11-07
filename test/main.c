3dfb08ad12bfff3a63254736fd6cb9f8ef97940c
Bryan Quatremare <tiplouf76320@live.fr>
2016-11-07 11:05:34 +0100

    Ajout de la soustraction

--- test/main.c
+++ test/main.c
@@ -6,8 +6,20 @@ int addition (int a, int b)
     return a+b;
 }
 
+int soustraction (int a, int b)
+{
+    return a-b;
+}
+
 int main()
 {
-    printf("Voilà ton résultat fdp %d\n", adition(1,4));
+    int op;
+    printf("Choisis le signe de ton opération\n");
+    scanf("%d", &op);
+    if(op == +)
+
+        printf("Voilà ton résultat fdp %d\n", addition(1,4));
+    else
+        printf("Voilà ton résultat fdp %d\n", soustraction(1,4));
     return 0;
 }
